06806536733ef56ff17ef74390c510f6e9a67faf
Sare Soltani <sare.soltani96@yahoo.com>
2019-01-28 10:27:26 +0330

    progressing

---
+++ components/Main.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="main">
+    <div class="header">
+      <div class="logo container p-3 pt-5 mb-5 col-12">
+        <a href="https://www.lyft.com/drive-with-lyft"><img src="../assets/lyft-logo-png-4.png"></a>
+      </div>
+      <div class="SignUp container mt-5 ">
+        <h1 class="container col-12"> Drive toward what matters.</h1>
+        <p> Sign up to drive</p>
+        <p> Let's start with your phone number - we'll text you a code to verify your phone.</p>
+      </div>
+    </div>
+  </div>
+    <!--
+    <h1>{{ msg }}</h1>
+
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_  blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+    -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css";
+  @import "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+  @import "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css";
+  @import "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+
+.header{
+  /*height: 100px;*/
+  background-color: rgb(119, 94, 240);
+}
+.logo img{
+  width: 80px;
+}
+
+.SignUp{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: white;
+}
+h1{
+  font-size: 5em;
+  font-weight: bold;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
